15b1dff2fe1851b312305f652874bbf85ea261b5
tetianadoc <doctatyana1@gmail.com>
2021-10-04 17:49:08 +0200

    Entry point for application.

--- initial/src/main/java/com/example/restservice/RestServiceApplication.java
+++ initial/src/main/java/com/example/restservice/RestServiceApplication.java
@@ -3,6 +3,10 @@ package com.example.restservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Entry point for the application
+ */
+
 @SpringBootApplication
 public class RestServiceApplication {
 
